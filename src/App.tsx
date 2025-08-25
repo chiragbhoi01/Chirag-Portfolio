285ab7101834ef65f9a8b15bc4062692a778cd6e
chiragbhoi01 <kumarchirag8890@gmail.com>
2025-08-25 22:24:08 +0530

    feat: Add new components for Contact, Footer, Projects, and Skills; enhance App structure and improve About and Header components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Project from "./components/Projects";
+import Skills from "./components/Skills";
 
 function App() {
   return (
     <div className="min-h-screen bg-gradient-to-bl from-[#ffe4e6] to-[#ccfbf1] flex flex-col">
       <Header />
       <Home />
-      <About/>
+      <About />
+      <Skills />
+      <Project />
+      <Contact />
+      <Footer />
     </div>
-
   );
 }
 
